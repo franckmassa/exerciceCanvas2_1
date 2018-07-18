4b020c4b9bc58c36ae7c8d41d11e72c7bea34e5b
massa <massafranck@gmail.com>
2018-07-18 22:46:07 +0200

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var c = document.getElementById('canvas');
+var ctx = c.getContext('2d');
+
+//Boule de glace courbe quadratic
+ctx.beginPath();
+ctx.lineWidth="3";
+ctx.strokeStyle="black";
+ctx.moveTo(140,150);
+ctx.quadraticCurveTo(230,50,260,150);
+ctx.fillStyle = '#8A0908';
+ctx.fill();
+//cône
+ctx.beginPath();
+ctx.moveTo(140,150);
+ctx.lineTo(260,150);
+ctx.lineTo(200,300);
+ctx.closePath();
+ctx.fillStyle = '#AA6522';
+ctx.fill();
